6682e0145e1a06e0b868c5502ca7df2e6e1b2179
Jordan Santell <jsantell@gmail.com>
2012-01-28 21:20:31 -0500

    Create sessions with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_or_create_by(provider: auth["provider"], uid: auth["uid"])
+    user.update_info(auth)
     session[:user_id] = user.id
     redirect_to root_url, notice: "You are now logged in"
   end
