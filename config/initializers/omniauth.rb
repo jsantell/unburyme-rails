e217ceb315ca082e1e421a774ab3f8f5bbdda546
Jordan Santell <jsantell@gmail.com>
2012-01-28 19:19:42 -0500

    Add omniauth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['UNBURYME_TWITTER_CONSUMER_KEY'], ENV['UNBURYME_TWITTER_CONSUMER_SECRET']
+  provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :google, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+end
