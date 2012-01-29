5f8064bdd3e87590359d20d0899058c87056978f
Jordan Santell <jsantell@gmail.com>
2012-01-28 19:29:44 -0500

    Basic func for sessions, application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout Proc.new { |controller| controller.request.xhr? ? false : :main }
+
+  private
+  def current_user
+    @current_user ||= logged_in? ? User.find(session[:user_id]) :nil
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  helper_method :current_user, :logged_in?, :log_in_notification
 end
